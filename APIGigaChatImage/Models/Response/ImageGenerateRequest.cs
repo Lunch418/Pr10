a7b88e981a6a895cc02c09582e99fe856558533b
Артем Сыпачев <Lunch418@bk.ru>
2025-12-17 01:48:16 +0500

    Разработанный метод для получения токена пользователя. + Respsponse Сыпачев.

---
+++ APIGigaChatImage/Models/Response/ImageGenerateRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace APIGigaChatImage.Models.Request
+{
+    public class ImageGenerateRequest
+    {
+        public string model { get; set; } = "GigaChat";
+        public string prompt { get; set; }
+        public StyleSettings style_settings { get; set; }
+        public bool with_description { get; set; } = false;
+    }
+
+    public class StyleSettings
+    {
+        public string style { get; set; } = "realistic";
+        public string color_palette { get; set; } = "vibrant";
+        public string aspect_ratio { get; set; } = "16:9";
+        public string theme { get; set; } = "neutral";
+        public string holiday { get; set; } = null;
+    }
+}
---
+++ APIGigaChatImage/Models/Response/ImageGenerateResponse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace APIGigaChatImage.Models.Response
+{
+    public class ImageGenerateResponse
+    {
+        public List<Choice> choices { get; set; }
+        public long created { get; set; }
+        public string model { get; set; }
+        public Usage usage { get; set; }
+
+        public class Choice
+        {
+            public Message message { get; set; }
+            public string finish_reason { get; set; }
+            public int index { get; set; }
+        }
+
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+            public List<ImagePart> image_parts { get; set; }
+        }
+
+        public class ImagePart
+        {
+            public string type { get; set; }
+            public ImageData image { get; set; }
+        }
+
+        public class ImageData
+        {
+            public string image_id { get; set; }
+            public string data { get; set; }
+        }
+
+        public class Usage
+        {
+            public int prompt_tokens { get; set; }
+            public int completion_tokens { get; set; }
+            public int total_tokens { get; set; }
+        }
+    }
+}
