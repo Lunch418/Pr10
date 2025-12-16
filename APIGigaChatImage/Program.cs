10bb3c58c8c1118a6e75ad2a31ce40d835a6fb2f
Артем Сыпачев <Lunch418@bk.ru>
2025-12-17 01:53:11 +0500

    Доделать. Надо

--- APIGigaChatImage/Program.cs
+++ APIGigaChatImage/Program.cs
@@ -8,8 +8,11 @@ namespace APIGigaChatImage
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string GigaChatClientId = "019b285e-c114-72cc-b636-b11cfb5fd760";
+        public static string GigaChatAuthorizationKey = "MDE5YjI4NWUtYzExNC03MmNjLWI2MzYtYjExY2ZiNWZkNzYwOjllOGM5OGRjLTc5MDgtNDAwMi1hN2QwLWNmZWU0ZmE2NGM0OQ==";
+        static async Task Main(string[] args)
         {
+            
         }
     }
 }
