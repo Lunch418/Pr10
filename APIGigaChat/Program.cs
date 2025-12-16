cc2cfb6ca6904ca49586b3c87f76cfd807a05931
Артем Сыпачев <Lunch418@bk.ru>
2025-12-16 23:34:24 +0500

    Разработанный метод для получения токена пользователя. Сыпачев

--- APIGigaChat/Program.cs
+++ APIGigaChat/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using APIGigaChat.Models.Response;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,45 @@ namespace APIGigaChat
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string ClientId = "019b285e-c114-72cc-b636-b11cfb5fd760";
+        public static string AuthorizationKey = "MDE5YjI4NWUtYzExNC03MmNjLWI2MzYtYjExY2ZiNWZkNzYwOjllOGM5OGRjLTc5MDgtNDAwMi1hN2QwLWNmZWU0ZmE2NGM0OQ==";
+        static async Task Main(string[] args)
         {
+        }
+        public static async Task<string> GetToken(string rqUID, string bearer)
+        {
+            string ReturnToken = null;
+            string Url = "https://ngw.devices.sberbank.ru:9WU3/api/v2/oauth";
+
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, cert, chain, ss1PolicyErrors) => true;
+
+                using (HttpClient Clien = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("RqUID", rqUID);
+                    Request.Headers.Add("Authorization", $"Bearer {bearer}");
+
+                    var Data = new List<KeyValuePair<string, string>> {
+                new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+            };
+
+                    Request.Content = new FormUrlEncodedContent(Data);
+
+                    HttpResponseMessage Response = await Clien.SendAsync(Request);
+
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        ResponseToken Token = JsonConvert.DeserializeObject<ResponseToken>(ResponseContent);
+                        ReturnToken = Token.access_token;
+                    }
+                }
+            }
+            return ReturnToken;
         }
     }
 }
